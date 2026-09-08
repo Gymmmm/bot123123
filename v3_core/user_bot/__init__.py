191d6abc7d26f55bbd5a3646741cf0fa03a30615
Gymmmm <73370798+Gymmmm@users.noreply.github.com>
2026-09-08 22:31:42 +0700

    v3: start user bot contract package

---
+++ v3_core/user_bot/__init__.py
@@ -0,0 +1,5 @@
+"""V3 User Bot contracts and read-only public inventory adapters."""
+
+from .deeplink import PublicChannelRoute, parse_channel_start_payload
+
+__all__ = ["PublicChannelRoute", "parse_channel_start_payload"]
